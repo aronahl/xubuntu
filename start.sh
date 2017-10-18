2d4ad19f81efa2095913d5b58e0156f2e01894e7
aronahl <aronahl@hotmail.com>
2017-10-18 01:16:14 -0400

    Retry session start.

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 xpra start --bind-tcp=0.0.0.0:9999 --html=on --dpi=192 --start="Xnest -geometry 1600x800 :1"
-sleep 10
 export DISPLAY=:1 
-exec xfce4-session
+while ! xfce4-session
+do
+    sleep 1
+done
